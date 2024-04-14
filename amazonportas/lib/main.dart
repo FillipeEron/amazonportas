4e7cb410c515935824e90a99e1ae157698191e72
FillipeEron <fillipeeron@gmail.com>
2024-04-14 18:43:55 -0300

    add second screen and animation transition

--- amazonportas/lib/main.dart
+++ amazonportas/lib/main.dart
@@ -1,62 +1,113 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(MyApp());
-  /*runApp(const MaterialApp(
-      title: "Login",
-      home: Scaffold(
-        body: Center(
-          child: AssetImage(assetName),
-        ),
-      )));*/
 }
+
+final GoRouter _router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+        path: '/',
+        builder: (BuildContext context, GoRouterState state) {
+          return Login();
+        },
+        routes: <RouteBase>[
+          GoRoute(
+            path: 'secondscreen',
+            pageBuilder: (BuildContext context, GoRouterState state) {
+              return CustomTransitionPage(
+                  child: SecondScreen(),
+                  transitionsBuilder: (BuildContext context,
+                      Animation<double> animation,
+                      Animation<double> secondaryAnaminaion,
+                      Widget child) {
+                    return FadeTransition(
+                      opacity: CurveTween(curve: Curves.bounceInOut)
+                          .animate(animation),
+                      child: child,
+                    );
+                  });
+            },
+            builder: (BuildContext context, GoRouterState state) {
+              return SecondScreen();
+            },
+          ),
+        ]),
+  ],
+);
 
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Login",
-      home: Scaffold(
-        body: structLogin,
+    return MaterialApp.router(
+      routerConfig: _router,
+    );
+  }
+}
+
+class Login extends StatelessWidget {
+  Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.only(
+            left: 70,
+            right: 70,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Image.asset("assets/amazonportas.png"),
+              const TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Login",
+                ),
+              ),
+              const TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Senha",
+                ),
+              ),
+              ElevatedButton(
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.blueGrey,
+                    padding: const EdgeInsets.all(16.0),
+                    textStyle: const TextStyle(fontSize: 20.0),
+                  ),
+                  onPressed: () => context.go('/secondscreen'),
+                  child: const Text("Acessar"))
+            ],
+          ),
+        ),
       ),
     );
   }
 }
 
-final structLogin = Center(
-  child: Container(
-    padding: const EdgeInsets.only(
-      left: 70,
-      right: 70,
-    ),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      mainAxisSize: MainAxisSize.max,
-      children: <Widget>[
-        Image.asset("assets/amazonportas.png"),
-        const TextField(
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: "Login",
-          ),
+class SecondScreen extends StatelessWidget {
+  SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => context.go('/'),
+          child: const Text('Go back!'),
         ),
-        const TextField(
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: "Senha",
-          ),
-        ),
-        TextButton(
-            style: TextButton.styleFrom(
-              foregroundColor: Colors.white,
-              padding: const EdgeInsets.all(16.0),
-              textStyle: const TextStyle(fontSize: 20.0),
-            ),
-            onPressed: null,
-            child: const Text("Acessar"))
-      ],
-    ),
-  ),
-);
+      ),
+    );
+  }
+}
